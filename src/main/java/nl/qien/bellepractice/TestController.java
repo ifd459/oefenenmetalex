cca958419f1e2ba4f67edcda3510c910132a216f
ifd459 <isabella.f.deane@gmail.com>
2020-08-10 10:38:33 +0200

    update

--- src/main/java/nl/qien/bellepractice/TestController.java
+++ src/main/java/nl/qien/bellepractice/TestController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
         @GetMapping("/all")
         public String getTest() {
-            throw new NullPointerException();
+//            throw new NullPointerException();
+            return "aliens! run!";
         }
 
         @PostMapping("/add")
